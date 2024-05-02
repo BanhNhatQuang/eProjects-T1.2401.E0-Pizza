53ac15fb6042f4108b83d17c1f122d144ce5c98f
Nhat Quang Banh <banhnhatquang@gmail.com>
2024-05-02 21:03:24 +0700

    Product phases 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import Product from './pages/Product';
+import { useEffect, useState } from 'react';
 function App() {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch("/data/products.json")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+    console.log("hello");
   return (
     <div className="container">
       <Header></Header>
       <Routes>
-        <Route></Route>
+        <Route element={<Product setProducts={setProducts} products={products}></Product>} path='/Product'></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Product.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+function Product(props) {
+    const { products, setProducts } = props
+    return (
+        <div>
+            <h1>hello</h1>
+            {/* <div>{setProducts}</div> */}
+        </div>
+    );
+}
+
+export default Product;
